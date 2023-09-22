95266d33fdb19a0c978e84a7d02cdcf214f26bd5
Marco Catoi <catoi.work@gmail.com>
2023-09-22 10:28:49 -0300

    Update EXE 065 - Aula 14.py
    Redone from scratch
    Refeito do zero

--- Curso em video/EXE 065 - Aula 14.py
+++ Curso em video/EXE 065 - Aula 14.py
@@ -2,27 +2,34 @@
 #  No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
 #  O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
-num = int
-maior = int(0)
-menor = int
-cont = int(0)
+dec = 'S'
+lista = str('')
 soma = int(0)
+cont = int(0)
+mayor = int(0)
+minor = int(0)
 media = float
-dec = str
-
 while dec != 'N':
-    num = input('Digite um numero: ')
-    num = int
-    soma = soma + int(num)
-    cont = cont + 1
-    media = soma/cont
-    if num > maior:
-        maior = int(num)
-    elif num < menor:
-        menor = int(num)
-    else:
-        menor = maior
-    dec = input('Deseja continuar digitando? S/N:  ').upper
-print('O maior entre os numeros digitados foi {} e o menor foi {}.'
-      'A média entre os numeros digitados foi de {}.format(maior, menor, media)')
+    num = int(input('Digite um numero inteiro: '))
+    cont += 1
+    soma += num
 
+    if lista == '':
+        lista = '{}'.format(str(num))
+    else:
+        lista = '{}, {}'.format(lista, str(num))
+
+    if num > mayor:
+        mayor = num
+    if num < minor:
+        minor = num
+    if minor == 0:
+        minor = num
+
+    dec = str(input('Você deseja continuar digitando?(S/N) :')).upper()
+media = soma/cont
+print('\n'
+      'Os numeros digitados está na lista abaixo:\n'
+      '|{}|\n'.format(lista))
+print('A média dos numeros digitados é {:.2f}.\n'.format(media))
+print('O maior numero digitado é {} e o menor é {}.'.format(mayor, minor))
