6e542823b2ae41c1669efafa84588a10a5c6df94
Marco Catoi <catoi.work@gmail.com>
2023-09-26 10:28:51 -0300

    Create EXE 066 - Aula 15.py
    Como dito no Commit do EXE 64, essa seria a forma correta de fazer o exercício usando o comando 'break' para interromper o laço.
    
    As stated in EXE 64 Commit, this would be the correct way to do the exercise using the 'break' command to interrupt the loop.

---
+++ Curso em video/EXE 066 - Aula 15.py
@@ -0,0 +1,15 @@
+# Crie um programa que leia números inteiros pelo teclado. 
+# O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. 
+# No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
+n = int(0)
+cont = int(0)
+soma = int(0)
+
+while cont != '999':
+    cont = input('Digite um numero para ser somado.(Quando quiser encerrar digite o numero "999":')
+    if cont == '999':
+        break
+    soma = soma + int(cont)
+    n = n + 1
+print('A quantidade de numeros digitados é: {} '
+      'e o resultado da soma dos numeros digitados é: {}'.format(n, soma))
